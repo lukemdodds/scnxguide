f8312d80f5602b966fdb0c74dc1a88d6f20cdc2f
lukemdodds <100125521+lukemdodds@users.noreply.github.com>
2022-02-21 11:36:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,576 @@
+/*function toggleMenu(){
+	var menuToggle = document.querySelector('.toggle');
+menuToggle.classList.toggle('active');
+var menu = document.querySelector(".menu");
+    menu.classList.toggle("active");
+}	*/
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+function brutalf() {
+  /* Get the text field */
+  var copyText = document.getElementById("brutalf");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+} 
+
+function emails() {
+  /* Get the text field */
+  var copyText = document.getElementById("emails");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+} 
+
+
+function phssce() {
+  /* Get the text field */
+  var copyText = document.getElementById("phssce");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+} 
+
+
+function maldom() {
+  /* Get the text field */
+  var copyText = document.getElementById("maldom");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+} 
+
+function malip() {
+  /* Get the text field */
+  var copyText = document.getElementById("malip");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+} 
+
+function raregeo() {
+  /* Get the text field */
+  var copyText = document.getElementById("raregeo");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+} 
+
+function landsa() {
+  /* Get the text field */
+  var copyText = document.getElementById("landsa");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+} 
+
+function susex() {
+  /* Get the text field */
+  var copyText = document.getElementById("susex");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+} 
+
+function rapfkp() {
+  /* Get the text field */
+  var copyText = document.getElementById("rapfkp");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function spat() {
+  /* Get the text field */
+  var copyText = document.getElementById("spat");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function dacwap() {
+  /* Get the text field */
+  var copyText = document.getElementById("dacwap");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function rhabalc() {
+  /* Get the text field */
+  var copyText = document.getElementById("rhabalc");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function anocosnp() {
+  /* Get the text field */
+  var copyText = document.getElementById("anocosnp");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function fttrgd() {
+  /* Get the text field */
+  var copyText = document.getElementById("fttrgd");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function uopphe() {
+  /* Get the text field */
+  var copyText = document.getElementById("uopphe");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function rpubaont() {
+  /* Get the text field */
+  var copyText = document.getElementById("rpubaont");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+
+function sapil() {
+  /* Get the text field */
+  var copyText = document.getElementById("sapil");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function wabtm() {
+  /* Get the text field */
+  var copyText = document.getElementById("wabtm");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function riltdc() {
+  /* Get the text field */
+  var copyText = document.getElementById("riltdc");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function sinope() {
+  /* Get the text field */
+  var copyText = document.getElementById("sinope");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function pi() {
+  /* Get the text field */
+  var copyText = document.getElementById("pi");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function uaufvua() {
+  /* Get the text field */
+  var copyText = document.getElementById("uaufvua");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function ambnpua() {
+  /* Get the text field */
+  var copyText = document.getElementById("ambnpua");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+
+function uoecas() {
+  /* Get the text field */
+  var copyText = document.getElementById("uoecas");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+
+function ppth() {
+  /* Get the text field */
+  var copyText = document.getElementById("ppth");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function peoldap() {
+  /* Get the text field */
+  var copyText = document.getElementById("peoldap");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+
+function amodtfdns() {
+  /* Get the text field */
+  var copyText = document.getElementById("amodtfdns");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function aafmciad() {
+  /* Get the text field */
+  var copyText = document.getElementById("aafmciad");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function plmoon() {
+  /* Get the text field */
+  var copyText = document.getElementById("plmoon");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function anope() {
+  /* Get the text field */
+  var copyText = document.getElementById("anope");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function anonsa() {
+  /* Get the text field */
+  var copyText = document.getElementById("anonsa");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+
+function sinosaa() {
+  /* Get the text field */
+  var copyText = document.getElementById("sinosaa");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function ppe() {
+  /* Get the text field */
+  var copyText = document.getElementById("ppe");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+function ttka() {
+  /* Get the text field */
+  var copyText = document.getElementById("ttka");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+
+function ppsfeia() {
+  /* Get the text field */
+  var copyText = document.getElementById("ppsfeia");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+/* Login page */
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate() {
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "ftechadmin" && password == "@dmin123"){
+window.location = "index.html"; // Redirecting to other page.
+return false;
+}
+
+else{
+attempt --;// Decrementing by one.
+alert("You have "+attempt+" attempts left.");
+// Disabling fields after 3 attempts.
+if( attempt == 0) {
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
+
+for (i = 0; i < coll.length; i++) {
+coll[i].addEventListener("click", function() {
+this.classList.toggle("active");
+var content = this.nextElementSibling;
+if (content.style.maxHeight){
+content.style.maxHeight = null;
+} else {
+content.style.maxHeight = content.scrollHeight + "px";
+}
+});
+}
+
+function prepare_link() {
+          var url_param = document.getElementById('url_param');
+          var target_link = document.getElementById('target_link');
+
+          if ( ! url_param.value ) {
+               return false;  
+          }
+
+          target_link.href = target_link.href + '/' + escape(url_param.value);
+		  target_link.href = target_link.href.replace(/%20/g, "")
+     }
+	 
+	 function prepare_link1() {
+          var url_param = document.getElementById('url_param');
+          var target_link = document.getElementById('target_link1');
+
+          if ( ! url_param.value ) {
+               return false;  
+          }
+
+          target_link.href = target_link.href + escape(url_param.value);
+		  target_link.href = target_link.href.replace(/%20/g, "")
+     }
+	 
+	  function prepare_link2() {
+          var url_param = document.getElementById('url_param');
+          var target_link = document.getElementById('target_link2');
+
+          if ( ! url_param.value ) {
+               return false;  
+          }
+
+          target_link.href = target_link.href + escape(url_param.value) + '&run=toolpage';
+     }
+	 
+	 function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("policylist");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("button")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+	 
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show").style.transition = "all 2s";
+}
+
+
+	 
+	 
+	 
+
